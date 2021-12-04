97026e4987092c16e1a502abd5b6167972066153
Mark Disbrow <disbrm1@nationwide.com>
2021-12-04 01:41:02 -0500

    Day 2 done

---
+++ Day2/instructions.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Char
+import Data.Text (isPrefixOf, pack)
+
+toInt :: String -> Int
+toInt = read
+
+data Instruction = Up Int | Down Int | Left Int | Right Int | Forward Int | Back Int | Unknown deriving (Show)
+
+parseInstruction :: String -> Instruction
+parseInstruction line = case words line of
+                          [direction, distance] -> if isPrefixOf "forward" (pack direction)
+                                                   then Forward (toInt distance)
+                                                   else if isPrefixOf "up" (pack direction)
+                                                   then Up (toInt distance)
+                                                   else if isPrefixOf "down" (pack direction)
+                                                   then Down (toInt distance)
+                                                   else Unknown
+                          otherwise -> Unknown
+
+getInstructions :: IO [Instruction]
+getInstructions = do
+    input <- getContents
+    let ls = map parseInstruction $ lines input
+    return ls
+
+data Position = Position { depth :: Int, forward :: Int } deriving (Show)
+startingPosition = Position 0 0
+
+processInstructions :: [Instruction] -> Position
+processInstructions = foldl (\p i -> case i of
+                               (Up amount)      -> Position ((depth p) - amount) (forward p)
+                               (Down amount)    -> Position ((depth p) + amount) (forward p)
+                               (Forward amount) -> Position (depth p) ((forward p) + amount)
+                               otherwise        -> p
+                               ) startingPosition
+
+main = do
+    instructions <- getInstructions
+    let result = processInstructions instructions
+    let product = (forward result) * (depth result)
+    putStrLn $ "Final product is: " ++ (show product)
